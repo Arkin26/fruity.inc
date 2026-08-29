66f384e5cce2fe3220f59a41785075b82cf2ff93
Arkin26 <arkinutmal@gmail.com>
2026-08-29 08:10:31 +0530

    Move shop below hero, full-width sales and collection cards with shorter height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
     <>
       <Hero />
       <div className="bg-black">
+        <ShopSection />
         <FeaturedCards />
         <AboutNewsSection />
-        <ShopSection />
         <SocialsSection />
       </div>
     </>
